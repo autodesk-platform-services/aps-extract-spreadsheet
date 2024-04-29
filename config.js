af315598afa764fcdf7117dc42c2703cceff0c1c
Varun Patil <varun.patil578@gmail.com>
2024-04-29 13:31:04 +0530

    migrated to latest version of APS SDK.

--- config.js
+++ config.js
@@ -1,32 +1,16 @@
-/////////////////////////////////////////////////////////////////////
-// Copyright (c) Autodesk, Inc. All rights reserved
-// Written by APS Partner Development
-//
-// Permission to use, copy, modify, and distribute this software in
-// object code form for any purpose and without fee is hereby granted,
-// provided that the above copyright notice appears in all copies and
-// that both that copyright notice and the limited warranty and
-// restricted rights notice below appear in all supporting
-// documentation.
-//
-// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
-// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
-// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.
-// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
-// UNINTERRUPTED OR ERROR FREE.
-/////////////////////////////////////////////////////////////////////
+require('dotenv').config();
 
-'use strict'; // http://www.w3schools.com/js/js_strict.asp
+let { APS_CLIENT_ID, APS_CLIENT_SECRET, APS_BUCKET, PORT } = process.env;
+if (!APS_CLIENT_ID || !APS_CLIENT_SECRET) {
+    console.warn('Missing some of the environment variables.');
+    process.exit(1);
+}
+APS_BUCKET = APS_BUCKET || `${APS_CLIENT_ID.toLowerCase()}-basic-app`;
+PORT = PORT || 8080;
 
 module.exports = {
-  // set enviroment variables or hard-code here
-  credentials: {
-    client_id: process.env.APS_CLIENT_ID || '<replace with your consumer key>',
-    client_secret: process.env.APS_CLIENT_SECRET || '<replace with your consumer secret>',
-  },
-  bucket: process.env.APS_BUCKET,
-  // Required scopes for your application on server-side
-  scopeInternal: 'data:read data:write data:create data:search bucket:create bucket:read bucket:update bucket:delete',
-  // Required scope of the token sent to the client
-  scopePublic: 'data:read viewables:read'
+    APS_CLIENT_ID,
+    APS_CLIENT_SECRET,
+    APS_BUCKET,
+    PORT
 };
--- start.js
+++ start.js
@@ -1,34 +1,7 @@
-/////////////////////////////////////////////////////////////////////
-// Copyright (c) Autodesk, Inc. All rights reserved
-// Written by APS Partner Development
-//
-// Permission to use, copy, modify, and distribute this software in
-// object code form for any purpose and without fee is hereby granted,
-// provided that the above copyright notice appears in all copies and
-// that both that copyright notice and the limited warranty and
-// restricted rights notice below appear in all supporting
-// documentation.
-//
-// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
-// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
-// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.
-// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
-// UNINTERRUPTED OR ERROR FREE.
-/////////////////////////////////////////////////////////////////////
-
-const path = require('path');
 const express = require('express');
-
-if (process.env.APS_CLIENT_ID == null || process.env.APS_CLIENT_SECRET == null || process.env.APS_BUCKET == null) {
-    console.error('Some of the required environment variables are missing.');
-    process.exit(1);
-}
+const { PORT } = require('./config.js');
 
 let app = express();
-app.use('/', express.static(path.join(__dirname, 'public')));
-app.set('port', process.env.PORT || 3000);
+app.use(express.static('public'));
 app.use(require('./routes/api.js'));
-let server = app.listen(app.get('port'), function () {
-    console.log('Starting at ' + (new Date()).toString());
-    console.log('Server listening on port ' + server.address().port);
-});
+app.listen(PORT, function () { console.log(`Server listening on port ${PORT}...`); });
